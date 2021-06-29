23304658d62330bd22c98ba2b0105e6ae8253ad7
sergjun <serrgio.madureira@gmail.com>
2021-06-29 18:51:21 -0300

    finalizado uma parte do projeto, criado as funcionalidade des Create Read e Delete

---
+++ README.md
@@ -0,0 +1,30 @@
+
+# Agenda telefonica
+
+Um CRUD em javascript criado com a ideia de ser uma agenda telefonica, pretendo ir aperfeiçoando o codigo conforme melhoro meus estudo em Javascript
+
+
+## Tech Stack
+
+**Client:** Javascript
+
+**Server:** SheetDB
+
+  
+## Authors
+
+- [@sergjun](https://www.github.com/sergjun)
+
+  
+## Roadmap
+
+- Implementada as funções de Create Read e Delete
+
+- Implementar a função de dar update
+
+- Mostrar a lista na tela (no momento apenas esta a mostra nos consoles)
+
+- Melhorar a interface visual da lista (não consegui pensar em nenhuma ideia inicial para a identidade visual da mesma)
+
+- Criar uma função que faça o request da api e a retorne para não ter que usar o fetch em cada uma das funções
+
---
+++ script.js
@@ -0,0 +1,109 @@
+document.querySelector("#form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    postAgenda()
+    
+})
+
+const campoNome = document.querySelector("#name")
+const campoTelefone = document.querySelector("#telefone")
+const mostrarLista = document.querySelector("#mostrar-lista")
+const listElement = document.querySelector("ul");
+
+
+mostrarLista.addEventListener("click", () => { baseAgenda()})
+
+// GET na API
+
+async function baseAgenda ()
+{
+    try {
+
+        
+        const apiColetada = await fetch ("https://sheetdb.io/api/v1/kmaxmkc0yg9ta")
+        const apiTransformada = await apiColetada.json()    
+        
+        console.log(apiTransformada)
+
+        return apiTransformada   
+        
+    }
+    catch (error) {
+        console.log(error);
+      }
+
+      
+}
+
+
+//  POST na API 
+
+async function postAgenda ()
+{
+    try {
+
+        const nomeValue = campoNome.value
+        const telefoneValue = campoTelefone.value 
+        const apiColetada = await fetch ("https://sheetdb.io/api/v1/kmaxmkc0yg9ta", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            
+            body: JSON.stringify({ data: { firstname: nomeValue, telefone: telefoneValue  }}),
+        });
+        
+        const apiTransformada = await apiColetada.json()
+        
+
+        console.log(apiTransformada)
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+
+   
+    // DELETE na API
+
+   async function deletaAgenda ()
+{
+    try {
+
+        const firstName = "sergio" 
+        const apiColetada = await fetch (`https://sheetdb.io/api/v1/kmaxmkc0yg9ta/firstname/${firstName}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            
+            // body: JSON.stringify({ data: { id: 2, firstname: 'usuario2' }}),
+        });
+        
+        const apiTransformada = await apiColetada.json()
+        console.log(apiTransformada)
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+
+ //  UPDATE na API
+async function updateAgenda ()
+{
+    try {
+        const apiColetada = await fetch (`https://sheetdb.io/api/v1/kmaxmkc0yg9ta/firstname/lika`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const apiTransformada = await apiColetada.json()
+        console.log(apiTransformada)
+        
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
